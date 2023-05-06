c113b14ddbfb85bc25cec6327dd82d4f28ec9f93
threehHong <honghyohun@naver.com>
2023-05-06 17:36:31 +0900

    react - voca

---
+++ 02_react_voca/src/App.js
@@ -0,0 +1,15 @@
+import Day from './component/Day';
+import DayList from './component/DayList';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <DayList />
+      <Day />
+    </div>
+  );
+}
+
+export default App;
