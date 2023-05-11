eeeebafd7562996e98d9915044616d0749ef9c1f
threehHong <honghyohun@naver.com>
2023-05-11 21:53:23 +0900

    feat: 기존 내용 삭제, user 배열 추가

--- velopert/begin-react/src/App.js
+++ velopert/begin-react/src/App.js
@@ -1,33 +1,41 @@
+// useRef의 다른 용도 
+/* DOM을 선택하는 용도 외에도 컴포넌트 안에서 조회 및 수정 할 수 있는
+변수를 관리하는 것. */
+
 import './App.css';
-import Hello from "./Hello";
-import Wrapper from "./Wrapper";
-import Counter from "./Counter";
-import InputSample from './InputSample';
+import React, { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    // 나중에 구현 할 배열에 항목 추가하는 로직
+    // ...
+
+    nextId.current += 1;
   }
 
   return (
-    <Wrapper>
-      <UserList />
-
-      <InputSample />
-
-      <Counter />
-
-      <Hello name="react" color="red" isSpecial={true}/>
-      <Hello color="pink" />
-
-      <div style={style}> {name} </div>
-      <div className="gray-box"> </div>
-    </Wrapper>
+    <UserList users={users} />
   );
 }
 
