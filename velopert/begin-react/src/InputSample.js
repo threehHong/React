0b453b7e52f4a73eb4c439061354c22dc1755d11
threehHong <honghyohun@naver.com>
2023-05-11 11:47:42 +0900

    feat: 여러개의 input 상태 관리하기

--- velopert/begin-react/src/InputSample.js
+++ velopert/begin-react/src/InputSample.js
@@ -1,61 +1,46 @@
-import { useState, useRef } from "react";
+import React, { useState } from 'react';
 
 function InputSample() {
-    // const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  })
 
-    const [inputs, setInputs] = useState({
-        name: '',
-        nickname: ''
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출(이론에 정리한 내용 참고 - 구조 분해 할당)
+
+  const onChange = (e) => {
+    const { value, name } = e.target; 
+    // 우선 e.target 에서 name 과 value 를 추출해서 value와 name에 저장.
+    // 입력하고 있는 <input name="name" placeholder="이름" value=""> 가 출력됨(여기서 value와 name에 저장되 값을 value와 name에 저장).
+
+    // console.log(e.target);
+
+    setInputs({
+        ...inputs, // 기존의 input 객체를 복사한 뒤(스프레드 연산자)
+        [name]: value // const { value, name } = e.target; 여기서 name에 입력된 키 값이 [name]에 들어가고 value에 입력된 값을 [name]에 저장한다.
     });
 
-    const nameInput = useRef();
+  };
 
-    // 비구조화 할당을 통해 값 추출
-    const { name, nickname } = inputs;
+  const onReset = () => {
+    setInputs({
+        name: '',
+        nickname: '',
+    })
+  };
 
-    const onChange = (e) => {
-        const {value, name} = e.target; // 우선 e.target에서 name과 value를 추출
 
-        console.log(e.target);
-
-        setInputs({
-            ...inputs, // 기존의 input 객체를 복사한 뒤
-            [name]: value // name 키를 가진 값을 value로 설정
-        })
-    };
-
-    const onReset = () => {
-        setInputs({
-            name: '',
-            nickname: '',
-        })
-
-        nameInput.current.focus();
-
-    }
-
-    return (
-        <div>
-            {/* 1.input 상태 관리하기 
-                value={text}를 설정하지 않으면 초기화 버튼을 눌러도
-                input에 입력된 내용이 초기화 되지 않는다. */}
-            {/* <input onChange={onChange} value={text} /> */}
-            
-
-            {/* 2. 여러개의 input 상태 관리하기 */}
-            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput}/>
-
-            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
-
-            <button onClick={onReset}> 초기화 </button>
-            
-            <div>
-                <b> 값 : </b>
-                {name} ({nickname})
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>값: </b>
+        {name} ({nickname})
+      </div>
+    </div>
+  );
 }
 
 export default InputSample;
-
