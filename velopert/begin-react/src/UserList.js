063e1f521db1e8b82dd39f93bfab8872d0615173
threehHong <109531669+threehHong@users.noreply.github.com>
2023-05-12 15:28:32 +0900

    feat: 등록, 수정, 삭제

--- velopert/begin-react/src/UserList.js
+++ velopert/begin-react/src/UserList.js
@@ -1,36 +1,31 @@
-function User({user}) {  
+import React, { useEffect } from 'react';
+
+function User({user, onRemove, onToggle}) {  
     return (
         <div>
-            <b> {user.username} </b>
+            <b
+              style={{
+                cursor: 'pointer',
+                color: user.active ? 'green' : 'black'
+              }}
+              onClick={() => onToggle(user.id)}
+            > 
+              {user.username} 
+            </b>
+            &nbsp;
             <span> ({user.email}) </span>
+            <button onClick={() => onRemove(user.id)}> 삭제 </button>
         </div>
     )
 }
 
-function UserList() {
-    /* const users = [
-      {
-        id: 1,
-        username: 'velopert',
-        email: 'public.velopert@gmail.com'
-      },
-      {
-        id: 2,
-        username: 'tester',
-        email: 'tester@example.com'
-      },
-      {
-        id: 3,
-        username: 'liz',
-        email: 'liz@example.com'
-      }
-    ]; */
-    
+function UserList({users, onRemove, onToggle}) {
     return (
       <div>
         {/* users를 user로 받아오는 것 */}
+
         {users.map(user => {
-            return <User user={user} key={user.id} />
+            return <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
         })}
 
         {/* <User user={users[0]} />
