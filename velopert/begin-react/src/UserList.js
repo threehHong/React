2842002e193e3f6a103b9bb237d5576438ad35df
threehHong <honghyohun@naver.com>
2023-05-15 18:30:08 +0900

    feat: useRef

--- velopert/begin-react/src/UserList.js
+++ velopert/begin-react/src/UserList.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
-function User({ user }) {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>({user.email})</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black' // user.active의 값이 true이면 green 아니면 black
+        }}
+        onClick = {() => onToggle(user.id)}
+      > {user.username} </b> 
+      <span> ({user.email}) </span>
+      <button onClick={() => onRemove(user.id)}> 삭제 </button>
     </div>
   );
 }
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map(user => (
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
       ))}
     </div>
   );
