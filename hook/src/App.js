d07428ede1580a433d6fe6a1d5325b1f0d44f0de
threehHong <honghyohun@naver.com>
2023-05-15 13:18:02 +0900

    feat: useRef

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,13 +1,12 @@
 // useState, useRef
 
 import './App.css';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 
 function App() {
-  /* 
-  // 1. useRef
-  const [count, setCount] = useState(0);
+  // 1. useRef(변수 관리)
+  /* const [count, setCount] = useState(0);
   const countRef = useRef(0);
 
   console.log(countRef);
@@ -18,8 +17,9 @@ function App() {
     console.log("Ref : ", countRef.current);
   } */
 
-  // 2. useRef
-  const [renderer, setRenderer] = useState(0);
+
+  // 2. useRef(변수 관리)
+  /* const [renderer, setRenderer] = useState(0);
   const countRef = useRef(0);
   let countVar = 0;
 
@@ -39,23 +39,40 @@ function App() {
 
   const printResults = () => {
     console.log(`ref : ${countRef.current}, var : ${countVar}`)
-  }
+  } */
+
+
+  // 3. useRef(변수 관리)
+  /* const [count, setCount] = useState(1);
+  const renderCount = useRef(0);
+
+
+  useEffect(() => { // useEffect 렌더링 될 때 마다 실행되는 훅
+    console.log('렌더링', renderCount.current);
+    renderCount.current = renderCount.current + 1;
+  }) */
 
   return (
     <div> 
-      {/* 1. useRef
-      <p> State : {count} </p>
+      {/* 1. useRef(변수 관리) */}
+      {/* <p> State : {count} </p>
       <p> Ref : {countRef.current} </p>
       <button onClick={increaseCountState}> State 올려 </button>
       <button onClick={increaseCountRef}> Ref 올려 </button> */}
 
 
-      <p> Ref : {countRef.current} </p>
+      {/* 2. useRef(변수 관리) */}
+      {/* <p> Ref : {countRef.current} </p>
       <p> Var : {countVar} </p>
       <button onClick={doRendering}> 렌더 </button>
       <button onClick={increaseRef}> Ref 올려 </button>
       <button onClick={increaseVar}> Var 올려 </button>
-      <button onClick={printResults}> Ref Var 값 출력 </button>
+      <button onClick={printResults}> Ref Var 값 출력 </button> */}
+
+
+      {/* 3. useRef(변수 관리) */}
+      {/* <p> Count: {count} </p>
+      <button onClick={() => setCount(count + 1)}> 올려 </button> */}
     </div>
   );
 }
@@ -63,35 +80,38 @@ function App() {
 export default App;
 
 
-/* 
-useRef
-
-const ref = useRef(value);
-useRef(value);를 호출하면 ref 객체를 반환
-ref 객체는 { current: value } 와 같다
-useRef(value는)에서 괄호안의 value는 current에 저장됨
-컴포넌트가 계속해서 렌더링 되어도 언마운트 되기 전까지는 값을 그대로 유지할 수 있음
-current - 현재 참조하고 있는 Element
-ref에 값을 저장하면 값은 저장되지만 렌더링 되지 않는다(증가하는 값을 계속해서 화면에 보여주지 않아도 될 경우 useState 대신 useRef를 사용하여
-성능에 도움을 줄 수 있다).
-
-
-const ref = useRef("hi") -> {current: "hi"} -> ref.current = "hi"
-const ref = useRef("hello") -> {current: "hello"} -> ref.current = "hello"
-const ref = useRef("world") -> {current: "world"} -> ref.current = "world"
-
-
-저장 공간으로서의 기능
-Ref의 변화 -> No 렌더링 -> 변수들의 값 유지
-State의 변화 -> 렌더링 -> Ref의 값 유지(컴포넌트 마운트 해지 전까지)
-
-DOM 요소에 접근 기능 
-ref를 통해 DOM 요소에 접근 가능(Document.querySelector 생각하기).
-ex. input을 클릭하지 않아도 포커스를 주고싶을 때.
-
-
-*/
 
 /* 
+1. useRef
+- const ref = useRef(value);
+  useRef(value);를 호출하면 ref 객체를 반환
+  ref 객체는 ref = { current: value } 와 같다. 따라서 ref.current로 입력된 값을 불러오거나 저장할 수 있다.
+
+
+- ref에 값을 저장하면 값은 저장되지만 렌더링 되지 않는다(증가하는 값을 계속해서 화면에 보여주지 않아도 될 경우 useState 대신 useRef를 사용하여
+  성능에 도움을 줄 수 있다).
+ 
+
+- const ref = useRef("hi") -> {current: "hi"} -> ref.current = "hi"
+  const ref = useRef("hello") -> {current: "hello"} -> ref.current = "hello"
+  const ref = useRef("world") -> {current: "world"} -> ref.current = "world"
+
+
+- 저장 공간으로서의 기능
+  Ref의 변화 -> No 렌더링 -> 변수들의 값 유지
+  State의 변화 -> 렌더링 -> Ref의 값 유지(컴포넌트 마운트 해지 전까지)
+
+
+- DOM 요소에 접근 기능 
+  ref를 통해 DOM 요소에 접근 가능(Document.querySelector 생각하기).
+  ex. input을 클릭하지 않아도 포커스를 주고싶을 때.
+
+
+2. useRef(DOM 요소에 접근)
+- current - 현재 참조하고 있는 Element
+
+
+3. 기타
 useState()가 변경되면 해당 컴포너트가 재 렌더링 된다.
+
 */
