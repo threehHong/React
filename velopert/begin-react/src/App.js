cae391f533146de404686edd5d7fb91486c5ba7a
threehHong <109531669+threehHong@users.noreply.github.com>
2023-05-19 14:52:21 +0900

    Update App.js

--- velopert/begin-react/src/App.js
+++ velopert/begin-react/src/App.js
@@ -1,9 +1,9 @@
-import React, { useRef, useState, useMemo, useCallback } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
 function countActiveUsers(users) {
-    console.log('활성 사용자 수를 세는중...');
+  console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
 
@@ -12,20 +12,14 @@ function App() {
     username: '',
     email: ''
   });
-  
   const { username, email } = inputs;
-  
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
-      setInputs({
-        ...inputs,
-        [name]: value
-      });
-    },
-    [inputs]
-  );
-
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -48,8 +42,7 @@ function App() {
   ]);
 
   const nextId = useRef(4);
-
-  const onCreate = useCallback(() => {
+  const onCreate = () => {
     const user = {
       id: nextId.current,
       username,
@@ -62,41 +55,33 @@ function App() {
       email: ''
     });
     nextId.current += 1;
-  }, [users, username, email]);
+  };
 
-  const onRemove = useCallback(
-    id => {
-      // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-      // = user.id 가 id 인 것을 제거함
-      setUsers(users.filter(user => user.id !== id));
-    },
-    [users]
-  );
-  const onToggle = useCallback(
-    id => {
-      setUsers(
-        users.map(user =>
-          user.id === id ? { ...user, active: !user.active } : user
-        )
-      );
-    },
-    [users]
-  );
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
+  const onRemove = id => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter(user => user.id !== id));
+  };
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+  const count = countActiveUsers(users);
   return (
     <>
-      <CreateUser 
+      <CreateUser
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
       <div>활성사용자 수 : {count}</div>
     </>
-  )
+  );
 }
 
 export default App;
-
