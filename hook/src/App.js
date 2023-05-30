4f00abbade8da3599797a58d09e02ed443963303
threehHong <honghyohun@naver.com>
2023-05-30 23:41:03 +0900

    custom hook

--- hook/src/App.js
+++ hook/src/App.js
@@ -4,7 +4,7 @@ Custom Hook
 
 2.
 function use훅이름() {
-  원하는 기능 로직 or 다른 hook 사용
+  원하는 기능 로직 or 다른 hook
 }
 */
 
@@ -20,14 +20,19 @@ function App() {
   const [inputValue, setInputValue] = useState('');
   
   const handleChange = (e) => {
-    setInputValue(e.tartget.value);
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    alert(inputValue);
+    setInputValue('');
   }
 
   return (
     <Div>  
       <h1> useInput </h1>
       <input value={inputValue} onChange={handleChange}/>
-      <button> 확인 </button>
+      <button onClick={handleSubmit}> 확인 </button>
     </Div>
   );
 }
