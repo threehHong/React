8d7f6a318439314562180da369ef5c6cbb1ca913
threehHong <109531669+threehHong@users.noreply.github.com>
2023-06-02 16:40:06 +0900

    velopert 복습 1-16

--- velopert/begin-react/src/App.js
+++ velopert/begin-react/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const onChange = e => {
     const { name, value } = e.target;
-    setInputs({
+    setInputs({ 
       ...inputs,
       [name]: value
     });
@@ -23,17 +23,20 @@ function App() {
     {
         id: 1,
         username: 'velopert',
-        email: 'public.velopert@gamil.com'
+        email: 'public.velopert@gamil.com',
+        active: true
     },
     {
         id: 2,
         username: 'tester',
-        email: 'tester@example.com'
+        email: 'tester@example.com',
+        active: false
     },
     {
         id: 3,
         username: 'liz',
-        email: 'liz@example.com'
+        email: 'liz@example.com',
+        active: false
     },
   ]);
 
@@ -48,7 +51,10 @@ function App() {
       email
     };
 
-    setUsers([...users, user]);
+    /* console.log(user.username, user.email); */
+
+    /* setUsers([...users, user]); */ // 이 부분 탐구 및 정리하기.
+    setUsers(users.concat(user)); // setUsers([...users, user]); 와 같은 내용
 
     setInputs({
       username: '',
@@ -58,16 +64,34 @@ function App() {
     nextId.current += 1;
   };
 
+  const onRemove = id => {
+    // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id가 id인 것을 제거함.
+    setUsers(users.filter(user => user.id !== id));
+  }
+
+  const onToggle = id => {
+    setUsers(
+      users.map(user => 
+        user.id === id ? {...user, active: !user.active} : user 
+        // 클릭한 users 객체의 id 번호와 users 객체의 id가 일치하면 대괄호 내용 실행
+        // user.active가 false일 경우 true로 바꿔줌
+      )
+    );
+  }
+
   return (
-    <>
+    <div
+      style={{padding: '20px'}}
+    >
       <CreateUser 
         username={username}
         email={email}
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
-    </>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+    </div>
   )
 }
 
--- velopert/begin-react/src/component/UserList.js
+++ velopert/begin-react/src/component/UserList.js
@@ -1,23 +1,38 @@
 import React from 'react';
 
-function User({user}) {
-    return (
+function User({ user, onRemove, onToggle }) {
+    return ( 
         <div>
-            <b> {user.username} </b> <span> ({user.email}) </span>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+                onClick={() => onToggle(user.id)}
+            > {user.username} </b> 
+            &nbsp;
+            <span> ({user.email}) </span>
+            <button onClick={() => onRemove(user.id)}> 삭제 </button>
         </div>
     )
 }
 
 
-function UserList({ users }) {
+function UserList({ users, onRemove, onToggle }) {
 
     return (
         <div>
           {users.map((user) => (
-            <User user={user} key={user.id} />
+            <User 
+                user={user} 
+                key={user.id} 
+                onRemove={onRemove} 
+                onToggle={onToggle}
+            />
           ))}
         </div>
     );
 }
 
 export default UserList
+
