24b097801fe623731c6661f403f1600fc523011e
threehHong <109531669+threehHong@users.noreply.github.com>
2023-06-03 16:56:03 +0900

    custom hook

--- hook/src/component/useFetch.js
+++ hook/src/component/useFetch.js
@@ -7,7 +7,7 @@ export function useFetch(baseUrl, initialType) {
     fetch(baseUrl + '/' + type) // ajax 생각하기
     .then((res) => res.json()) // fetch로 가져온 데이터를 json 형태로 변환
     /* .then((res) => console.log(res)); */ // 변환된 값을 콘솔에 출력
-    .then((res) => setData(res));
+    .then((res) => setData(res)); // 반환된 값을 data 변수에 저장
   }
 
   useEffect(() => {
