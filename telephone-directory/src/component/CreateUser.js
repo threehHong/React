34f6ca58dd3709722f9980adc14456ac49fe4747
threehHong <109531669+threehHong@users.noreply.github.com>
2023-06-13 16:54:12 +0900

    telephone-directory

---
+++ telephone-directory/src/component/CreateUser.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function CreateUser({ username, email, onChange, onCreate }) {
+  return (
+    <div>
+      <input
+        name="username"
+        placeholder="이름"
+        onChange={onChange}
+        value={username}
+      />
+      <input
+        style={ {margin: "0 3px"} }
+
+        name="email"
+        placeholder="전화번호"
+        onChange={onChange}
+        value={email}
+      />
+      <button onClick={onCreate}> 등록 </button>
+    </div>
+  );
+}
+
+export default CreateUser;
+
