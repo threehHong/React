265149e79f1ba5f79409ea0aba1f21c1755c891f
threehHong <honghyohun@naver.com>
2023-06-16 11:55:57 +0900

    feat: Nav, Banner 컴포넌트 추가

--- a-z/netflix-clone/src/App.js
+++ a-z/netflix-clone/src/App.js
@@ -1,8 +1,11 @@
-import './App.css';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Banner />
     </div>
   );
 }
