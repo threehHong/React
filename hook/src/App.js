3c2bf387ec427fc47ed8327fa4f62fba055b19c9
threehHong <109531669+threehHong@users.noreply.github.com>
2023-06-17 10:26:02 +0900

    add: count app 코드 추가(연습)

--- hook/src/App.js
+++ hook/src/App.js
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 
-function Count () {
+function Count() {
   const [number, setNumber] = useState(0);
 
   const increment = () => {
-    setNumber(number + 1);
+    setNumber(number + 1);  
   }
 
   const decrement = () => {
-    setNumber(number + 1);
+    setNumber(number - 1);
   }
 
   return (
     <div>
-      <p> count : {number} </p>
-      <button onClick={increment}> Increment </button>
-      <button onClick={decrement} > Decrement </button>
+      <p> Count : {number} </p>
+      <button onClick={increment} > Increment </button>
+      <button onClick={decrement}> Decrement </button>
     </div>
-  ) 
+  )
 }
 
 function App() {
@@ -29,34 +29,3 @@ function App() {
 export default App;
 
 
-
-/*
-// props를 사용한 방법(위의 방법이 더 좋다 - props를 사용하지 않아도 되므로). 
-function Count ( {number, setNumber} ) {
-  const [number, setNumber] = useState(1);
-
-  const increment = () => {
-    setNumber(number + 1);
-  }
-
-  const decrement = () => {
-    setNumber(number + 1);
-  }
-
-  return (
-    <div>
-      <p> count : {number} </p>
-      <button onClick={increment}> Increment </button>
-      <button onClick={decrement} > Decrement </button>
-    </div>
-  ) 
-}
-
-function App() {
-  const [number, setNumber] = useState(1);
-
-  return (
-    <Count number={number} setNumber={setNumber} />
-  )
-} 
-*/
