9e883dc4b670ff0d9cfa1e82c72a9c3c37edafed
threehHong <109531669+threehHong@users.noreply.github.com>
2023-06-22 21:38:41 +0900

    feat: counter app - TDD

--- a-z/counter-app/src/App.test.js
+++ a-z/counter-app/src/App.test.js
@@ -1,58 +1,53 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
 // 숫자가 0에서 부터 시작하는지 테스트 코드
-test('the counter starts at 0', () => {
-  render(<App />);
-  const counterElement = screen.getByTestId("counter");
-  expect(counterElement).toHaveTextContent(0);
+test("the counter starts at 0", () => {
+    render(<App />);
+    const counterElement = screen.getByTestId("counter");
+    expect(counterElement).toHaveTextContent(0);
 });
 
-
 // 마이너스 버튼 테스트 코드
-test('minus button has correct text', () => {
-  render(<App />);
-  const buttonElement = screen.getByTestId("minus-button");
-  expect(buttonElement).toHaveTextContent("-");
-})
-
+test("minus button has correct text", () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId("minus-button");
+    expect(buttonElement).toHaveTextContent("-");
+});
 
 // 플러스 버튼 테스트 코드
-test('plus button has correct text', () => {
-  render(<App />);
-  const buttonElement = screen.getByTestId("plus-button");
-  expect(buttonElement).toHaveTextContent("+"); 
-})
-
+test("plus button has correct text", () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId("plus-button");
+    expect(buttonElement).toHaveTextContent("+");
+});
 
 // 플러스 버튼이 눌렸을 때, 숫자가 1증가하는지 테스트 코드
-test('When the + button is pressed, the counter changes to 1', () => {
-  render(<App />);
-  const buttonElement = screen.getByTestId("plus-button");
-  fireEvent.click(buttonElement);
+test("When the + button is pressed, the counter changes to 1", () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId("plus-button");
+    fireEvent.click(buttonElement);
 
-  const counterElement = screen.getByTestId("counter");
-  expect(counterElement).toHaveTextContent(1);
-})
-
+    const counterElement = screen.getByTestId("counter");
+    expect(counterElement).toHaveTextContent(1);
+});
 
 // on/off 버튼이 파란색 버튼인지 확인하는 코드
-test('on/off button has blue color', () => {
-  render(<App />);
-  const buttonElement = screen.getByTestId("on/off-button");
-  expect(buttonElement).toHaveStyle({backgroundColor: "blue"});
-})
-
+test("on/off button has blue color", () => {
+    render(<App />);
+    const buttonElement = screen.getByTestId("on/off-button");
+    expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
+});
 
 // on/off 버튼이 클릭 됐을 때, -,+ 버튼이 눌러지는 것을 막는지 테스트 하는 코드
 test("Prevent the -,+ button from being pressed when the on/off button is clicked", () => {
-  render(<App />);
+    render(<App />);
 
-  const onOffButtonElement = screen.getByTestId("on/off-button");
-  fireEvent.click(onOffButtonElement);
+    const onOffButtonElement = screen.getByTestId("on/off-button");
+    fireEvent.click(onOffButtonElement);
 
-  const plusButtonElement = screen.getByTestId("plus-button");
-  expect(plusButtonElement).toBeDisabled();
+    const plusButtonElement = screen.getByTestId("plus-button");
+    expect(plusButtonElement).toBeDisabled();
 });
 
 // test case의 구조
