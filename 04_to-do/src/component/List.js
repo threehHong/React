bbcb60c9a6ea725a110ffbd0e9d0a274309d9085
threehHong <honghyohun@naver.com>
2023-06-27 11:41:46 +0900

    feat: 할 일 완료 여부 체크 기능 추가

--- 04_to-do/src/component/List.js
+++ 04_to-do/src/component/List.js
@@ -25,13 +25,29 @@ export default function List({ todo, setTodo }) {
   const handleComplete = () => {
     const updatedTodo = todo.map(data => {
       if (data.id === editItemId) {
-        // 이 밑에 구문 이해하기.
         return { ...data, title: newTodo };
+        /* 
+        구문 설명
+        editItemId과 일치하는 data 객체를 찾아서 해당 객체의 title 속성을 newTodo로 업데이트한 새로운 객체를 반환하는 구문
+        결과적으로 다른 객체들은 그대로 있고 반환된 새로운 객체만 업데이트된 updatedTodo 배열을 생성.
+        */
       }
+
       return data;
     });
     setTodo(updatedTodo);
     setEditItemId(false);
+  }
+
+  const toggleCompleteLine = (id) => {
+    const updatedTodo = todo.map(data => {
+      if (data.id === id) {
+        return { ...data, completed: !data.completed}
+      }
+      return data;
+    })
+
+    setTodo(updatedTodo);
   }
 
   return (
@@ -51,7 +67,7 @@ export default function List({ todo, setTodo }) {
                 :
 
                 <>
-                  {data.title} &nbsp;
+                  <span onClick={() => toggleCompleteLine(data.id)} style={{ textDecoration: data.completed ? 'line-through' : 'none' }} > {data.title} </span> &nbsp;
                   <button onClick={() => handleModify(data.id, data.title)}> 수정 </button> &nbsp;
                   <button onClick={() => handleClick(data.id)}> 삭제 </button>
                 </>
