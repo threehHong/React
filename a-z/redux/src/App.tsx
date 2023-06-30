95fc9e034425025da4976257f63e1024b6880b31
threehHong <honghyohun@naver.com>
2023-06-30 13:00:25 +0900

    feat: counter app

---
+++ a-z/redux/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+type Props = {
+  value: number;
+  onIncrement: () => void;
+  onDecrement: () => void;
+}
+
+function App({value, onIncrement, onDecrement}: Props) {
+  return (
+    <div className="App">
+      clicked: {value} times
+
+      <button onClick={onIncrement}>
+        +
+      </button>
+
+      <button onClick={onDecrement}>
+        -
+      </button>
+    </div>
+  );
+}
+
+export default App;
