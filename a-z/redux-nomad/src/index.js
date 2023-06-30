0e2ec6b1a1d51f7ac0ea815d84a47013b247eea2
threehHong <109531669+threehHong@users.noreply.github.com>
2023-06-30 18:20:52 +0900

    feat: action, dispatch 추가

--- a-z/redux-nomad/src/index.js
+++ a-z/redux-nomad/src/index.js
@@ -17,8 +17,12 @@ const countStore = createStore(reducer);
 
 
 // reducer() - createStore 저장된 데이터를 수정하는 함수.
-const countModifier = () => {
-  return "hello"
+/* action - 함수를 부를 때 쓰는 두번째 parameter 혹은 argument
+          - countModifier와 소통하기 위한 도구 */
+const countModifier = (count = 1, action) => {
+  console.log(action)
+
+  return count
 };
 
 // createStore() - data를 저장하는 장소 + createStore()는 reducer()를 요구한다.
@@ -29,3 +33,7 @@ console.log(countStore); // dispatch, subscribe, getState, replaceReduce
 
 // getState() - 데이터를 가져오는 함수
 console.log(countStore.getState()); // hello
+
+
+// countModifier에게 action을 보내는 방법
+countStore.dispatch({ type: "HELLO" })
