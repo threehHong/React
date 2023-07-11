3da11db360aa6af4236061ba3f1ea74af170aff5
threehHong <honghyohun@naver.com>
2023-07-11 23:43:00 +0900

    소플의 처음 만난 리액트 실습 소스코드

---
+++ first-met-react-practice-v18-master/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+const Button = () => (
+	<button> Hello world </button>
+)
+
+/* const Button = () => {
+	<button> Hello world </button>
+} */
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button />
+      </header>
+    </div>
+  );
+}
+
+export default App;
